4d092624597b4cae36dbe8d156f98284bd4858ae
mrcaseyrobison <104058949+mrcaseyrobison@users.noreply.github.com>
2022-06-15 15:19:20 -0700

    more styling

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var questionEl = document.getElementById("#question");
+var containerEl = document.getElementById(".container")
+var option1El = document.getElementById("#option-1");
+var option2El = document.getElementById("#option-2");
+var option3El = document.getElementById("#option-3");
+var option4El = document.getElementById("#option-4");
+
+
+
