55f93987b146259a96ac412c25601da638c77881
mrcaseyrobison <104058949+mrcaseyrobison@users.noreply.github.com>
2022-07-21 14:39:14 -0700

    added game functions and timer - untested

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Game Variables
 var score = 0;
-var questions = 0;
+var questionsIndex = 0;
 
 // Time Clock Variables
 var startTime = 120;
@@ -10,10 +10,26 @@ var timeDeduct = 5;
 // HTML Elements
 var timerEl = document.getElementById("timer");
 var timeLeftEl = document.getElementById("timeLeft");
-var quizBoxEl = document.getElementById("quizBox");
-var gameContentEl = document.getElementById("gameContent");
-var createUl = document.createElement("ul");
+var quizBoxEl = document.getElementById("quizBox"); // links to "quizBox" ID in HTML
+var gameContentEl = document.getElementById("gameContent"); // links to "gameContent" ID in HTML
+var createUl = document.createElement("ul"); // creates unordered list element in HTML
 
+// Event Listener to start the timer and start game
+timerEl.addEventListener("click", function() {
+  if (pauseInterval === 0) {
+      pauseInterval = setInterval(function() {
+        startTime--;
+        timeLeftEl.textContent = "Time: " + startTime;
+        if (startTime <= 0) {
+          clearInterval(pauseInterval);
+          finished();
+          timeLeftEl.textContent = "Game Over";
+        }
+      }, 1000);
+  }
+  displayQuiz(questionsIndex)
+}
+)
 // Questions Array
 var gameQuestions = [
   {
@@ -47,5 +63,24 @@ var gameQuestions = [
   answer: "A. Answer 1"
   }
 ]
+
+// Function to Display Q & A to HTML
+function displayQuiz(questions) {
+  quizBoxEl.innerHTML = "";
+  createUl.innerHTML = "";
+  for (var i = 0; i < gameQuestions.length; i++) {
+    var userQuestions = gameQuestions(questionsIndex).question;
+    var userAnswers = gameQuestions(questionIndex).choices;
+    quizBoxEl.textContent = userQuestions;
+    }
+  userAnswers.forEach(function(nextQuestion) {
+    var listItem = document.createElement("li")
+    listItem.textContent = nextQuestion;
+    quizBoxEl.appendChild(createUl);
+    createUl.appendChild(listItem);
+    listItem.addEventListener("click", (compare));
+  })
+};
+
 
 
